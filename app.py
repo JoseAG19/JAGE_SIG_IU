a2b8c1ae469c4a28aaa4ecea5b7e2b4bde7e9d2a
JoseAG19 <165836565+JoseAG19@users.noreply.github.com>
2024-04-09 14:26:47 -0500

    Update app.py1

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ with open('Mapa de Accidentalidad Vial Municipio de Medellín 2016.geojson', "r"
 
 st.title("Accidentalidad Municipio de Medellín 2016")
 
+st.image("Accidentes.jpg")
+
 st.write('Se entiende por accidente de tránsito  evento, generalmente involuntario, generado al menos por un un vehículo en movimiento, que causa daños a '
          'personas y bienes involucrados en él, e igualmente afecta la normal circulación de los vehículos que se movilizan por la vía o vías comprendidas en el' 
          'lugar o dentro de la zona de influencia del hecho0 (Ley 769 de 2002 - Código Nacional de Tránsito)'
