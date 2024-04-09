5238f65ae3072c2f802e20c98e0bd566c2c17284
JoseAG19 <165836565+JoseAG19@users.noreply.github.com>
2024-04-09 13:38:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -53,20 +53,39 @@ st.map(df_g)
 
 # Realizar un filtrado de los datos
 st.subheader('Filtrado')
-option_hour_min = st.selectbox('Selecciona filtro por Hora',
-                               ('08:00:00', '09:00:00', '10:00:00','11:00:00','12:00:00','13:00:00','14:00:00'),key='1')
-#option_hour_max = st.selectbox('Selecciona filtro por Hora',
-#                               ('08:00:00', '09:00:00', '10:00:00','11:00:00','12:00:00','13:00:00','14:00:00'),key='2')
-option_day = st.selectbox('Selecciona filtro por día',('LUNES', 'MARTES', 'MIÉRCOLES','JUEVES','VIERNES','SÁBADO','DOMINGO'))
-#df_filtrado = df_g.query('día == "MIÉRCOLES" and Hora >= "08:00:00" and Hora <= "10:00:00"')
-df_filtrado = df_g.query('día == @option_day and Hora >=  @option_hour_min ')
+
+# Primero, selecciona el filtro por día
+option_day = st.selectbox('Selecciona filtro por día', ('LUNES', 'MARTES', 'MIÉRCOLES', 'JUEVES', 'VIERNES', 'SÁBADO', 'DOMINGO'))
+
+# Luego, selecciona el filtro por hora mínimo
+option_hour_min = st.selectbox('Selecciona filtro por Hora Mínima',
+                               ('00:00:00', '01:00:00', '02:00:00', '03:00:00', '04:00:00', '05:00:00',
+                                '06:00:00', '07:00:00', '08:00:00', '09:00:00', '10:00:00', '11:00:00',
+                                '12:00:00', '13:00:00', '14:00:00', '15:00:00', '16:00:00', '17:00:00',
+                                '18:00:00', '19:00:00', '20:00:00', '21:00:00', '22:00:00', '23:00:00'), key='1')
+
+# Luego, selecciona el filtro por hora máximo
+option_hour_max = st.selectbox('Selecciona filtro por Hora Máxima',
+                               ('00:00:00', '01:00:00', '02:00:00', '03:00:00', '04:00:00', '05:00:00',
+                                '06:00:00', '07:00:00', '08:00:00', '09:00:00', '10:00:00', '11:00:00',
+                                '12:00:00', '13:00:00', '14:00:00', '15:00:00', '16:00:00', '17:00:00',
+                                '18:00:00', '19:00:00', '20:00:00', '21:00:00', '22:00:00', '23:00:00'), key='2')
+
+# Agregar el filtro por barrio
+option_neighborhood = st.selectbox('Selecciona filtro por Barrio', sorted(df_g['Barrio'].unique()))
+
+# Aplicar el filtro a los datos
+df_filtrado = df_g.query('Barrio == @option_neighborhood and día == @option_day and Hora >= @option_hour_min and Hora <= @option_hour_max')
+
+# Mostrar la tabla de datos filtrada
 st.dataframe(df_filtrado)
 
+# Intenta mostrar la cantidad de incidentes dentro del filtro
 try:
    st.metric("Cantidad de Incidentes dentro del filtro", df_filtrado.shape[0])
 except:
     pass
 
-
+# Mostrar el mapa con los datos filtrados
 st.map(df_filtrado)
 #st.write(df)
